ddf724e4f0716db41b271efd30c4c75af58667d4
Justin <justinreeves52@gmail.com>
2021-06-16 21:40:35 -0400

    chore

--- public/app.js
+++ public/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3306;
-const APIKEY = process.env.KEY;
+export const APIKEY = process.env.KEY;
 app.use(express.static('public'));
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
 })
 
-module.exports = {APIKEY};
 
